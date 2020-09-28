422f749e410e1203113c0518abdd034d1a7bc7b8
Sir-Dazzling <ottidan20@gmail.com>
2020-09-28 14:28:49 +0100

    set up project and dependencies

---
+++ src/main/java/com/example/denvato_polls_demo/denvato_polls_demo/DenvatoPollsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.denvato_polls_demo.denvato_polls_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DenvatoPollsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DenvatoPollsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/denvato_polls_demo/denvato_polls_demo/DenvatoPollsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.denvato_polls_demo.denvato_polls_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DenvatoPollsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
